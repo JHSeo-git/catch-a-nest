38ad97aaca6d78015b0648e3e1e92a51bc24e5a5
JHSeo <qnfqnfqnf@gmail.com>
2021-04-03 14:23:12 +0900

    [fix] prevent duplicated same toast

--- packages/catch-a-nest-frontend/src/hooks/useAppToast.ts
+++ packages/catch-a-nest-frontend/src/hooks/useAppToast.ts
@@ -1,15 +1,16 @@
-import React from 'react';
 import { toast } from 'react-toastify';
 
 type NotifyType = 'info' | 'success' | 'error';
 
 export default function useAppToast() {
   const notify = (text: string, notifyType?: NotifyType) => {
-    if (notifyType === 'info') return toast.info(text);
-    else if (notifyType === 'error') return toast.error(text);
-    else if (notifyType === 'success') return toast.success(text);
+    if (notifyType === 'info') return toast.info(text, { toastId: text });
+    else if (notifyType === 'error')
+      return toast.error(text, { toastId: text });
+    else if (notifyType === 'success')
+      return toast.success(text, { toastId: text });
 
-    return () => toast(text);
+    return () => toast(text, { toastId: text });
   };
 
   const clearAllToast = () => toast.dismiss();
