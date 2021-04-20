d540672fd0236c4bd3427dbd23e23d0730240bd4
JHSeo <qnfqnfqnf@gmail.com>
2021-04-20 19:47:41 +0900

    [추가] cors

--- packages/catch-a-nest-backend/src/app.ts
+++ packages/catch-a-nest-backend/src/app.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import logger from 'koa-logger';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 import router from './router';
 import jwtMiddleware from './lib/middlewares/jwtMiddleware';
 import addIPAddress from './lib/middlewares/addIPAddress';
@@ -8,8 +9,25 @@ import healthCheck from './lib/middlewares/healthCheck';
 
 const app = new Koa();
 
+const validOrigins = [
+  `http://localhost:3000`,
+  'https://catch-a-nest.vercel.app/',
+];
+const corsOptions: cors.Options = {
+  origin: (ctx) => {
+    const headerOrigin = ctx.headers.origin;
+    if (!headerOrigin) {
+      return '';
+    }
+    if (!validOrigins.includes(headerOrigin)) return '';
+    return headerOrigin;
+  },
+  credentials: true,
+};
+
 app.proxy = true;
 app.use(logger());
+app.use(cors(corsOptions));
 app.use(bodyParser());
 app.use(addIPAddress);
 app.use(jwtMiddleware);
