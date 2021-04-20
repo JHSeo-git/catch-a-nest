22915915db3ed04672b94f07ef216bcfa5825dae
JHSeo <qnfqnfqnf@gmail.com>
2021-04-20 23:27:12 +0900

    [Change] api 주소 변경

--- packages/catch-a-nest-frontend/src/lib/api/axiosClient.ts
+++ packages/catch-a-nest-frontend/src/lib/api/axiosClient.ts
@@ -5,8 +5,8 @@ const axiosClient = axios.create();
 // test : ec2-54-180-96-175.ap-northeast-2.compute.amazonaws.com
 axiosClient.defaults.baseURL =
   process.env.NODE_ENV === 'development'
-    ? 'ec2-54-180-96-175.ap-northeast-2.compute.amazonaws.com:5001'
-    : 'ec2-54-180-96-175.ap-northeast-2.compute.amazonaws.com:5001';
+    ? 'api.seonest.net'
+    : 'api.seonest.net';
 axiosClient.defaults.withCredentials = true;
 
 export default axiosClient;
