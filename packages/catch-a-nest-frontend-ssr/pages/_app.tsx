e06c185f5e47517c73a9ca4691ad6f5bf3ed0f63
JHSeo <qnfqnfqnf@gmail.com>
2021-07-09 15:18:05 +0900

    [Add] firebase analytics

--- packages/catch-a-nest-frontend-ssr/pages/_app.tsx
+++ packages/catch-a-nest-frontend-ssr/pages/_app.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { RecoilRoot } from 'recoil';
 import { css, Global } from '@emotion/react';
 import RecoilInitializer from '@/components/RecoilInitializer';
@@ -44,8 +45,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <DefaultSEO />
       <RecoilRoot>
-        {/* {NODE_ENV !== 'production' && <RecoilInitializer />} */}
-        <RecoilDebugObserver />
+        {NODE_ENV !== 'production' && <RecoilDebugObserver />}
+        <RecoilInitializer />
         <AppToastProvider />
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
@@ -55,6 +56,39 @@ function MyApp({ Component, pageProps }: AppProps) {
           </Hydrate>
         </QueryClientProvider>
       </RecoilRoot>
+      {NODE_ENV !== 'production' && (
+        <>
+          <Script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js" />
+          <Script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-analytics.js" />
+          <Script
+            defer
+            dangerouslySetInnerHTML={{
+              __html: `
+                // Your web app's Firebase configuration
+                // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+                var firebaseConfig = {
+                  apiKey: 'AIzaSyAVhsch2_emuZmD9KLh3kVkRr3rSUm7m6g',
+                  authDomain: 'catch-a-nest.firebaseapp.com',
+                  projectId: 'catch-a-nest',
+                  storageBucket: 'catch-a-nest.appspot.com',
+                  messagingSenderId: '581628041764',
+                  appId: '1:581628041764:web:622697f154d3faae6a16b1',
+                  measurementId: 'G-H82Q5FTZYK',
+                };
+                // Initialize Firebase
+                if(window?.firebase){
+                  if(!firebase.apps.length){
+                    firebase.initializeApp(firebaseConfig);                    
+                  } else {
+                    firebase.app();
+                  }
+                  firebase.analytics();
+                }
+              `,
+            }}
+          />
+        </>
+      )}
     </>
   );
 }
