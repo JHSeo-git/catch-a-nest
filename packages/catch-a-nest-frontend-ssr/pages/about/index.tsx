4239f143f278c71738b07760601d1465aa58d9f9
JHSeo <qnfqnfqnf@gmail.com>
2021-07-10 18:42:59 +0900

    [Add] About page

--- packages/catch-a-nest-frontend-ssr/pages/about/index.tsx
+++ packages/catch-a-nest-frontend-ssr/pages/about/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { css } from '@emotion/react';
 import AppLayout from '@/components/AppLayout';
 import PageSEO from '@/components/AppSEO/PageSEO';
+import palette from '@/lib/styles/palette';
 
 export type AboutPageProps = {};
 
@@ -10,17 +11,39 @@ const AboutPage = (props: AboutPageProps) => {
     <>
       <PageSEO title="About" description="about Seo Nest" />
       <AppLayout>
-        <div css={block}>
-          🙇‍♂️ 안녕하세요! JHSeo 입니다.
-          <br />
-          <br />
-          ❤️ 방문해 주셔서 감사합니다
-          <br />
-          <br />
-          👷 이 페이지는 아직 완성 되지 않았습니다.
-          <br />
-          <br />
-        </div>
+        <article css={block}>
+          <h1>
+            Hi👋 I&apos;m <strong>JHSeo</strong> .
+          </h1>
+          <section>
+            <h2>
+              <strong>seonest.net</strong> developed by ...
+            </h2>
+            <h3>Front-end</h3>
+            <ul>
+              <li>Typescript</li>
+              <li>React</li>
+              <li>Nextjs</li>
+              <li>Recoiljs</li>
+              <li>Vercel</li>
+              <li>Axios</li>
+              <li>@emotion</li>
+              <li>@toast-ui/editor</li>
+              <li>Markdown-it</li>
+            </ul>
+            <h3>Back-end</h3>
+            <ul>
+              <li>Nodejs</li>
+              <li>Koa</li>
+              <li>Mariadb</li>
+              <li>Typeorm</li>
+              <li>AWS-EC2</li>
+              <li>AWS-S3</li>
+              <li>PM2</li>
+              <li>Cloudflare</li>
+            </ul>
+          </section>
+        </article>
       </AppLayout>
     </>
   );
@@ -29,11 +52,36 @@ const AboutPage = (props: AboutPageProps) => {
 const block = css`
   background: #fff;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
   letter-spacing: 0.5px;
+
+  h1,
+  h2 {
+    strong {
+      color: ${palette.lightBlue[800]};
+    }
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  ul {
+    padding-left: 2rem;
+    list-style: square;
+
+    li {
+      &::marker {
+        font-size: 1rem;
+        color: ${palette.lightBlue[800]};
+      }
+    }
+    li + li {
+      margin-top: 0.5rem;
+    }
+  }
 `;
 
 export default AboutPage;
