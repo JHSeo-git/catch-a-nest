5ce07694a0ce32bd673b067ab23139cd99a8ed09
JHSeo <qnfqnfqnf@gmail.com>
2021-07-18 17:15:10 +0900

    [Fix] posts(or post) with temp

--- packages/catch-a-nest-frontend-ssr/lib/api/posts/getAllPostSlug.ts
+++ packages/catch-a-nest-frontend-ssr/lib/api/posts/getAllPostSlug.ts
@@ -1,6 +1,6 @@
 import client from '../client';
 
-export default async function getAllPostSlug(includeTemp = false) {
+export default async function getAllPostSlug(includeTemp = true) {
   const response = await client.get<string[]>('/api/posts/all-slug', {
     params: {
       include_temp: includeTemp,
