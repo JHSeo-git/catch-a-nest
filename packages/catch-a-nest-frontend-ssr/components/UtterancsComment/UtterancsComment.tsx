95804d012879c260765a3b14aa9b5203ae1d8b88
JHSeo <qnfqnfqnf@gmail.com>
2021-07-22 00:34:42 +0900

    [Add] comment loading style

--- packages/catch-a-nest-frontend-ssr/components/UtterancsComment/UtterancsComment.tsx
+++ packages/catch-a-nest-frontend-ssr/components/UtterancsComment/UtterancsComment.tsx
@@ -1,7 +1,9 @@
 // import appConfig from '@/config/app.config';
-import { pageFadeInStyle } from '@/lib/styles/animation';
+import { pageFadeInStyle, rotateAnimation } from '@/lib/styles/animation';
+import palette from '@/lib/styles/palette';
 import { css } from '@emotion/react';
 import { useUtterances } from '../../hooks/useUtterances';
+import AppIcon from '../AppIcon';
 
 export type UtterancsCommentProps = {};
 
@@ -10,7 +12,10 @@ const UtterancsComment = (props: UtterancsCommentProps) => {
 
   return (
     <div css={block}>
-      <section ref={ref} />
+      <div css={loading}>
+        <AppIcon name="spinner" />
+      </div>
+      <section css={commentBox} ref={ref} />
     </div>
   );
   // return (
@@ -34,10 +39,31 @@ const UtterancsComment = (props: UtterancsCommentProps) => {
 };
 
 const block = css`
+  position: relative;
   margin-top: 4rem;
-  transition: all 0.5s ease-in-out;
   ${pageFadeInStyle()};
+`;
 
+const loading = css`
+  position: absolute;
+  z-index: -1;
+  top: 5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  svg {
+    width: 3rem;
+    height: 3rem;
+    animation: ${rotateAnimation} 1s ease-in-out infinite;
+    color: ${palette.lightBlue[500]};
+  }
+`;
+
+const commentBox = css`
+  position: absolute;
+  background-color: white;
+  left: 0;
+  top: 0;
+  right: 0;
   .utterances {
     width: 100%;
     max-width: 100%;
