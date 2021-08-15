2c1d57ebe0fec07683ca143ea821ea474d867f63
JHSeo <qnfqnfqnf@gmail.com>
2021-08-15 21:26:15 +0900

    [Fix] sitemap url

--- packages/catch-a-nest-frontend-ssr/pages/server-sitemap.xml/index.tsx
+++ packages/catch-a-nest-frontend-ssr/pages/server-sitemap.xml/index.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const postSlugs = await getAllPostSlug();
 
   const fields: ISitemapField[] = postSlugs.map((slug) => ({
-    loc: `${appConfig.url}/${slug}/`,
+    loc: `${appConfig.url}/posts/${slug}/`,
     changefreq: 'daily',
     priority: 0.7,
   }));
