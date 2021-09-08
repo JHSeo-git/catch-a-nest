83b06a593124d6863635994ed5b1dadada1a3115
JHSeo <qnfqnfqnf@gmail.com>
2021-09-08 20:42:22 +0900

    Style error page darkmode

--- packages/catch-a-nest-frontend-ssr/components/AppError/AppError.tsx
+++ packages/catch-a-nest-frontend-ssr/components/AppError/AppError.tsx
@@ -1,3 +1,4 @@
+import { useThemeValue } from '@/lib/recoil/appState';
 import { pageFadeInStyle } from '@/lib/styles/animation';
 import palette from '@/lib/styles/palette';
 import { resetButton } from '@/lib/styles/reset/resetButton';
@@ -11,16 +12,17 @@ export type AppErrorProps = {
 };
 
 const AppError = ({ message, status }: AppErrorProps) => {
+  const theme = useThemeValue();
   const router = useRouter();
   const onClick = () => {
     router.back();
   };
 
   return (
-    <div css={block}>
+    <div css={block(theme === 'DARK')}>
       <div css={contentStyle}>
         <h1>{status}</h1>
-        <div css={splitter} />
+        <div css={splitter(theme === 'DARK')} />
         <p>{message}</p>
       </div>
       <div css={contentStyle}>
@@ -32,7 +34,7 @@ const AppError = ({ message, status }: AppErrorProps) => {
   );
 };
 
-const block = css`
+const block = (isDarkMode: boolean) => css`
   position: fixed;
   top: 0;
   left: 0;
@@ -49,10 +51,20 @@ const block = css`
   h1 {
     font-size: 2.5rem;
     color: ${palette.blueGrey[900]};
+
+    ${isDarkMode &&
+    css`
+      color: ${palette.grey[50]};
+    `}
   }
   p {
     font-size: 1rem;
     color: ${palette.blueGrey[700]};
+
+    ${isDarkMode &&
+    css`
+      color: ${palette.grey[200]};
+    `}
   }
 `;
 
@@ -61,14 +73,19 @@ const contentStyle = css`
   align-items: center;
 `;
 
-const splitter = css`
+const splitter = (isDarkMode: boolean) => css`
   margin: 0 1rem;
   width: 0.0625rem;
   height: 2rem;
   background-color: ${palette.blueGrey[700]};
+
+  ${isDarkMode &&
+  css`
+    background-color: ${palette.grey[200]};
+  `}
 `;
 
-const backButton = css`
+const backButton = (isDarkMode: boolean) => css`
   ${resetButton};
   cursor: pointer;
   color: ${palette.blueGrey[800]};
@@ -77,10 +94,22 @@ const backButton = css`
   border: 0.0625rem solid ${palette.blueGrey[500]};
   transition: all 0.2s ease-in-out;
 
+  ${isDarkMode &&
+  css`
+    color: ${palette.grey[100]};
+    border: 0.0625rem solid ${palette.blueGrey[300]};
+  `}
+
   &:hover,
   &:active {
     color: ${palette.lightBlue[800]};
     box-shadow: 0 0.25rem 0.5rem rgba(0 0 0 /11%);
+
+    ${isDarkMode &&
+    css`
+      color: ${palette.lightBlue[200]};
+      box-shadow: 0 0.25rem 0.5rem rgba(0 0 0 /30%);
+    `}
   }
 `;
 
