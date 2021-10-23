90deb486d39b3a57a870441bd702fea882cea5fe
JHSeo <qnfqnfqnf@gmail.com>
2021-10-24 00:08:09 +0900

    add component using when logged in

--- packages/catch-a-nest-frontend-ssr/pages/admin/index.tsx
+++ packages/catch-a-nest-frontend-ssr/pages/admin/index.tsx
@@ -4,13 +4,27 @@ import { useUserValue } from '@/lib/recoil/authState';
 import PageSEO from '@/components/SEO/PageSEO';
 import AppLayout from '@/components/AppLayout';
 import { styled } from '@stitches.js';
+import { useRouter } from 'next/router';
+import Button from '@/components/common/Button';
 
-export type AdminProps = {};
-
-function Admin(props: AdminProps) {
+function Admin() {
   const userState = useUserValue();
+  const { back } = useRouter();
 
-  if (userState) return null;
+  if (userState)
+    return (
+      <AppLayout>
+        <Box
+          css={{
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <h2>Already Logged In</h2>
+          <Button onClick={() => back()}>Go Back</Button>
+        </Box>
+      </AppLayout>
+    );
 
   return (
     <>
